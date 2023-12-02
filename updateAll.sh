3d6c5bee0c79e7f4ec82d9ef04f26c84d0ab8746
FH3095 <FH3095@users.noreply.github.com>
2023-12-02 02:11:59 +0100

    Remove submodule

--- updateAll.sh
+++ updateAll.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+echo "### UPDATE"
 git pull
-git submodule update --remote --rebase
+
+echo "### UPDATE ASN-IPs"
+if [ ! -d "asn-ip" ]; then
+	git clone https://github.com/ipverse/asn-ip.git asn-ip
+fi
+cd asn-ip
+git pull
+cd ..
+
+echo "### UPDATE CIDR"
 ./convertAsn.py
 ./blockAsn.py
 cp blockedAsns.cidr /etc/postfix/blockedAsns.cidr
